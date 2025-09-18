953c8094aa328f1476f7f20f86433bab62771a11
Tarun Kumar <tarun.25b01010853@abes.ec.in>
2025-09-18 23:25:27 +0530

    2nd code

---
+++ a1.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int s = 32;
+    int area = 32*32;
+    int cost = area * 7; //cost is 7m/sq.
+    cout << " Total cost is $ " << cost << endl;
+    return 0;
+}
